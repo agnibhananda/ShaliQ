0e4e6bd693d294b935373c486080055d43d720ef
Agnibha Nanda <agnibhananda@gmail.com>
2025-02-16 00:24:27 +0530

    i think that's enough for now will get back when i know more rust

--- libs/genetic-algo/src/lib.rs
+++ libs/genetic-algo/src/lib.rs
@@ -1,3 +1,7 @@
+use rand::RngCore;
+use rand::seq::SliceRandom;
+use rand::{Rng, RngCore};
+
 pub struct GeneticAlgo{
     pub fn evolve<I>(&self, population: &[I]) -> Vec<I>
     where
@@ -16,4 +20,61 @@ pub struct GeneticAlgo{
 
 pub trait Individual {
     fn fitness(&self) -> f32;
+}
+
+pub trait SelectionMethod {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I   
+    where
+        I: Individual;
+}
+
+pub struct RouletteWheelSelection;
+
+impl SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where
+        I: Individual,
+    {
+        population
+            .choose_weighted(rng, |individual| individual.fitness())
+            .expect("got an empty population")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    #[derive(Clone, Debug)]
+    struct TestIndividual {
+        fitness: f32,
+    }
+
+    impl TestIndividual {
+        fn new(fitness: f32) -> Self {
+            Self { fitness }
+        }
+    }
+
+    impl Individual for TestIndividual {
+        fn fitness(&self) -> f32 {
+            self.fitness
+        }
+    }
+    #[test]
+    fn roulette_wheel_selection() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+         let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+        let actual = RouletteWheelSelection::new().select(&mut rng, &population);
+
+        assert!(actual, &population[2]);
+    }
 }
